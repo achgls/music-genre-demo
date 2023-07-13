0bd7bf042721ab81d391665627fa87fc367e3037
Achille <achille.gellens@gmail.com>
2023-07-13 20:05:08 +0200

    update utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,15 @@
 import json
 import os.path
+from io import BytesIO
+from typing import Any
+
+import ffmpeg
 import streamlit as st
 
 import torch
+from pytube import YouTube
+
+import plotly.express as px
 
 from . import transforms
 from .models import cnn
@@ -66,7 +73,7 @@ def slice_audio(
         slice_duration: float = 4.0,
         overlap: float = 0.75,
         sample_rate: int = 22_050,
-        max_duration=60
+        max_duration=600,
 ):
     assert 0.0 <= overlap < 1.0
     offset = 0
@@ -114,6 +121,84 @@ def load_model_and_transform(exp_dir: str, checkpoint: str = "accuracy", model_t
 
 
 def get_activation(name, out):
-    def hook(model, input, output):
+    def hook(i, o, output):
         out[name] = output.detach()
     return hook
+
+
+def ffmpeg_reencode_from_file(f, target_ar: str = '22050'):
+    audio_stream, err = (
+        ffmpeg
+        .input(f, ac=1)
+        .output("pipe:", format='wav',
+                acodec='pcm_s16le',
+                ar=target_ar,
+                ac=1)  # Select WAV output format, and pcm_s16le auidio codec.
+        .run(capture_stdout=True)
+    )
+    audio_io = BytesIO(audio_stream)
+    return audio_io, err
+
+
+def ffmpeg_reencode_from_buffer(f, target_ar: str = '22050'):
+    audio_stream, err = (
+        ffmpeg
+        .input(f)
+        .output("pipe:", format='wav',
+                acodec='pcm_s16le',
+                ar=target_ar,
+                ac=1)  # Select WAV output format, and pcm_s16le auidio codec.
+        .run(capture_stdout=True)
+    )
+    audio_io = BytesIO(audio_stream)
+    return audio_io, err
+
+
+
+def wav_tensor_from_stream(s):
+    wav = torch.frombuffer(s, dtype=torch.int16).to(torch.float32)
+    if wav.dim() == 1:
+        wav = wav.unqueeze(0)
+    wav = wav[:1, :]
+    wav -= torch.mean(wav)
+    wav /= torch.max(torch.abs(wav))
+    return wav
+
+
+def get_audio_stream_from_youtube(url: str):
+    yt = YouTube(url)
+
+    stream_url = yt.streams.filter(only_audio=True).first().url  # Get the URL of the video stream
+
+    # Probe the audio streams (use it in case you need information like sample rate):
+    # probe = ffmpeg.probe(stream_url)
+    # audio_streams = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
+    # sr = audio_streams['sample_rate']
+
+    # Read audio into memory buffer.
+    # Get the audio using stdout pipe of ffmpeg sub-process.
+    # The audio is transcoded to PCM codec in WAC container.
+    audio_io, err = ffmpeg_reencode_from_file(stream_url, target_ar='22050')
+
+    return audio_io
+
+
+@st.cache_resource
+def feature_plot(key: Any):
+    embeddings = st.session_state["model_history"][key]["embeddings"]
+    pca = st.session_state["model_history"][key]["pca"]
+    scaler = st.session_state["model_history"][key]["scaler"]
+    labels = st.session_state["model_history"][key]["labels_str"]
+    genres = ('Blues', 'Classical', 'Country', 'Disco', 'Hiphop', 'Jazz', 'Metal', 'Pop', 'Reggae', 'Rock')
+
+    proj = pca.transform(scaler.transform(embeddings))
+    trues = labels
+    fig = px.scatter_3d(
+        x=proj[:, 0], y=proj[:, 1], z=proj[:, 2],
+        color=trues,
+        color_discrete_map={
+            k: v for k, v in zip(genres, px.colors.qualitative.Set3)
+        },
+    )
+    fig.update_traces(marker_size=4)
+    return fig
